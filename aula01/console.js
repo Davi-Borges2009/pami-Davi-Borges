48b7879cf99666c7b414813a677fdb17dc2134d3
unknown <davialmeidaborges53@gmail.com>
2025-03-25 16:21:22 -0300

    introdução e variáveis em javascript

---
+++ aula01/console.js
@@ -0,0 +1,9 @@
+console.log("Olá, mundo")
+
+var A = "oi"
+let B = "tudo bem?"
+const c = 3
+const d = 4
+console.log(A, B)
+console.log(A + B)
+console.log(c + d)
---
+++ aula02/variaveis.js
@@ -0,0 +1,16 @@
+var numero = 2
+let texto = "hello"
+const OqueEisso = ")"
+
+console.log("tipo var: ", numero)
+
+numero = 13
+
+console.log(texto + "o número agora é: ", numero)
+console.log("${texto} o número agora é", numero)
+
+texto = 69
+
+console.log(texto+numero)
+
+console.log(OqueEisso)
